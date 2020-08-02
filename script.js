2f8d36bcd9c13dc24d8fc9b2b32dba194c96d182
sawfly29 <sawfly@yandex.ru>
2020-08-02 18:22:21 +0300

    v 0.1 - Браузерная версия переделана под платформу Node JS. Добавлен отдельный модуль для работы со звуковыми дорожками.

--- script.js
+++ script.js
@@ -1,39 +1,89 @@
-let date = new Date();
+const player = require("node-wav-player");
+let date, dayOfWeek;
 
-let dayOfWeek = date.getDay();
+handler();
+setInterval(handler, 60000);
 
-if (dayOfWeek == 6 || dayOfWeek == 0){
-    alert ("Дорогой, иди домой, ведь сегодня выходной!");
+function currentTime() {
+  date = new Date();
+  dayOfWeek = date.getDay();
+  console.log(date);
 }
-else {
-    let start= new Audio('start.mp3');
-    let finish = new Audio ('finish.mp3');
-    let currentHours =date.getHours();
-    let currentMinutes=date.getMinutes()/100;
-    let time = currentHours+currentMinutes;
-    let s1=8.30;
-    let f1=9.15;
-    let s2=9.35;
-    let f2=10.20;
-    let s3=10.40;
-    let f3=11.25;
-    let s4=11.35;
-    let f4=12.20;
-    let s5=12.35;
-    let f5=13.20;
-    let s6=13.35;
-    let f6=14.20;
-    let s7=14.30;
-    let f7=15.15;
-    let s8=15.25;
-    let f8=16.10;
-    if (s1 == time || s2 == time || s3 == time || s4 == time || s5 == time || s6 == time || s7 == time || s8 == time) {start.play();}
-    if (f1 == time || f2 == time || f3 == time || f4 == time || f5 == time || f6 == time || f7 == time || f8 == time) {finish.play();}
+function finishLesson() {
+  player
+    .play({
+      path: "./Sounds/finish.wav",
+    })
+    .then(() => {
+      console.log("Звонок с урока!");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
+function startLesson() {
+  player
+    .play({
+      path: "./Sounds/start.wav",
+    })
+    .then(() => {
+      console.log("Звонок на урок!");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
+function handler() {
+  currentTime();
 
-/*
-|| s3 == time || s4 == time || s5 == time || s6 == time || s7 == time
-|| f3 == time || f4 == time || f5 == time || f6 == time || f7 == time
-*/
+  if (dayOfWeek == 6 || dayOfWeek == 0) {
+    console.log("Дорогой, иди домой, ведь сегодня выходной!");
+  } else {
+    let currentHours = date.getHours();
+    let currentMinutes = date.getMinutes() / 100;
+    let time = currentHours + currentMinutes;
+    let s1 = 8.3;
+    let f1 = 9.15;
+    let s2 = 9.35;
+    let f2 = 10.2;
+    let s3 = 10.4;
+    let f3 = 11.25;
+    let s4 = 11.35;
+    let f4 = 12.2;
+    let s5 = 12.35;
+    let f5 = 13.2;
+    let s6 = 13.35;
+    let f6 = 14.2;
+    let s7 = 14.3;
+    let f7 = 15.15;
+    let s8 = 15.25;
+    let f8 = 16.1;
+    if (
+      s1 == time ||
+      s2 == time ||
+      s3 == time ||
+      s4 == time ||
+      s5 == time ||
+      s6 == time ||
+      s7 == time ||
+      s8 == time
+    ) {
+      startLesson();
+    } else if (
+      f1 == time ||
+      f2 == time ||
+      f3 == time ||
+      f4 == time ||
+      f5 == time ||
+      f6 == time ||
+      f7 == time ||
+      f8 == time
+    ) {
+      finishLesson();
+    } else {
+      console.log(time);
+    }
+  }
+}
